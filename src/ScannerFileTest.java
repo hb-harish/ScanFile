795b86a0395bacf6a99b2d56d092094ba581e133
harish <hb.harishbalaji@gmail.com>
2016-09-14 16:59:41 -0400

    customer scan

--- src/ScannerFileTest.java
+++ src/ScannerFileTest.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.io.PrintWriter;
 
 public class ScannerFileTest {
 	public static void main(String[] args) {
@@ -9,7 +11,14 @@ public class ScannerFileTest {
 			Scanner sc = new Scanner(file);
 			sc.useDelimiter("\\t");
 			Customer c = new Customer();
+			LinkedList<Customer> cust = new LinkedList<Customer>();
 			//you must escape the slash of the tab 
+			PrintWriter writer=null;
+			try {
+			     writer = new PrintWriter(new File("/home/oracle/workspace/java/ScanFile/src/output.txt"));
+			 } catch (FileNotFoundException e) {
+			     System.out.println("File not found");
+			 }
 			while (sc.hasNextLine()) {
 				String i = sc.next();
 				c.setFullName(i);
@@ -32,10 +41,13 @@ public class ScannerFileTest {
 				i = sc.next();
 				c.setCompany(i);
 				System.out.println(c.printall());
-
-
+				cust.add(c);
+				writer.print(c.getFullName()+ " ");
+				writer.println(c.getEmail()); 
+					 
 			}
 			sc.close();
+			writer.close();
 		} 
 		catch (FileNotFoundException e) {
 			e.printStackTrace();
